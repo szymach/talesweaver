2822699ae940b81181d9d655a3930abf2dc0de06
Piotr Szymaszek <pszymaszek@gmail.com>
2015-12-24 01:29:30 +0100

    Added CKEditor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new FSi\Bundle\DataSourceBundle\DataSourceBundle(),
 
             # App
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new AppBundle\AppBundle(),
         );
