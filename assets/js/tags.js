78728610b40a437e9cdf28bf8619636ad2a89583
Piotr Szymaszek <pszymaszek@gmail.com>
2016-02-09 20:18:22 +0100

    Removed PHP asset management, added seperate views for CRUD and a script for CKEDITOR tags

---
+++ assets/js/tags.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.tag-add').on('click', function() {
+        surroundSelection(document.createElement("span"));
+    });
+});
+
+function getCKEditorSelection()
+{
+    var instanceId = $('.cke').attr('id').replace('cke_', '');
+    var editor = CKEDITOR.instances[instanceId];
+    var mySelection = editor.getSelection();
+
+    if (CKEDITOR.env.ie) {
+        mySelection.unlock(true);
+    }
+
+    return mySelection;
+}
+
+function surroundSelection(element)
+{
+    var selection = getCKEditorSelection();
+    var nativeSelection = selection.getNative();
+    var range = nativeSelection.getRangeAt(0).cloneRange();
+    range.surroundContents(element);
+    nativeSelection.removeAllRanges();
+    nativeSelection.addRange(range);
+    
+    if (CKEDITOR.env.ie) {
+        selection.lock();
+    }
+}
