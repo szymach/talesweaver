12614064fe37352a2d20862ec1d6b82f13b66e3a
Piotr Szymaszek <pszymaszek@gmail.com>
2016-03-27 21:54:59 +0200

    Finished transition to Webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,2 @@
 $(document).ready(function() {
-    $('.datagrid-delete').on('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        
-        var $this = $(this);
-        $.ajax({
-            method: "DELETE",
-            url: $(this).attr('href')
-        })
-        .success(function() {
-            $this.parents('tr').first().remove();
-        });
-    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: {
-        'app.js': "./assets/js",
-        css: ''
-    },
+    entry: [
+        'font-awesome-sass-loader',
+        'bootstrap-loader/extractStyles',
+        "./assets/js/tags.js"
+    ],
     output: {
-        path: 'web/js',
+        path: 'web/public',
         filename: "app.js"
     },
+    resolve: { extensions: [ '', '.js' ] },
+    plugins: [
+        new ExtractTextPlugin('app.css', { allChunks: true })
+    ],
     module: {
         loaders: [
-            { test: /\.scss$/, loader: "style!css!sass" },
-            { test: /app\.js$/, loader: "uglify" }
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!sass') },
+            { test: /.*\/assets\/.*\.js$/, loader: "uglify" },
+            { test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url" },
+            { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/, loader: 'file' },
+            { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
         ]
+    },
+    'uglify-loader': {
+        mangle: false
     }
 };
