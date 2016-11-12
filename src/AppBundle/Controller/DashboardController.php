250c90afe77202a5b8aef69c4ac8263adc8aba2f
Piotr Szymaszek <pszymaszek@gmail.com>
2016-11-12 14:09:21 +0100

    Latest standalone chapters on dashboard

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Repository\ChapterRepository;
 use AppBundle\Entity\Repository\SceneRepository;
 use Symfony\Component\Templating\EngineInterface;
 
@@ -16,15 +17,22 @@ class DashboardController
     private $templating;
 
     /**
+     * @var ChapterRepository
+     */
+    private $chapterRepository;
+
+    /**
      * @var SceneRepository
      */
     private $sceneRepository;
 
     public function __construct(
         EngineInterface $templating,
+        ChapterRepository $chapterRepository,
         SceneRepository $sceneRepository
     ) {
         $this->templating = $templating;
+        $this->chapterRepository = $chapterRepository;
         $this->sceneRepository = $sceneRepository;
     }
 
@@ -32,7 +40,10 @@ class DashboardController
     {
         return $this->templating->renderResponse(
             'base\dashboard.html.twig',
-            ['standaloneScenes' => $this->sceneRepository->findLatestStandalone()]
+            [
+                'standaloneChapters' => $this->chapterRepository->findLatestStandalone(),
+                'standaloneScenes' => $this->sceneRepository->findLatestStandalone()
+            ]
         );
     }
 }
