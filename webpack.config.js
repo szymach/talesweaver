93d7fb55d5ae0866cec339c5aaf8f5aaecdc8b86
Piotr Szymaszek <pszymaszek@gmail.com>
2017-01-06 17:03:49 +0100

    Split asset files

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,23 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        'font-awesome-sass-loader',
-        "bootstrap-loader",
-        "./assets/scss/base.scss",
-        "./assets/js/app.js",
-        "./assets/js/tags.js"
-    ],
+    entry: {
+        main: [
+            "./assets/scss/base.scss",
+            "./assets/js/app.js",
+            "./assets/js/tags.js"
+        ],
+        vendor: ['font-awesome-sass-loader', "bootstrap-loader"]
+    },
     output: {
         path: 'web/public',
-        filename: "app.js"
+        filename: '[name].js'
     },
     resolve: {
         extensions: [ '.js' ]
     },
     plugins: [
-        new ExtractTextPlugin({ filename: 'app.css', allChunks: true }),
+        new ExtractTextPlugin({ filename: '[name].css', allChunks: true }),
         new OptimizeCssAssetsPlugin(),
         new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }, mangle: false })
     ],
@@ -27,7 +28,7 @@ module.exports = {
             { test: /\.scss$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader'}) },
             { test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader" },
             { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/, loader: 'file-loader' },
-            { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports-loader?jQuery=jquery' },
+            { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports-loader?jQuery=jquery' }
         ]
     }
 };
