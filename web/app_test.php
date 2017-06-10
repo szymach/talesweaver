1476f88765a9fae4ff351de9d36bb64067b0e616
Piotr Szymaszek <pszymaszek@gmail.com>
2017-06-10 23:37:00 +0200

    Removed Behat config, added Codeception code coverate configuration

--- web/app_test.php
+++ web/app_test.php
@@ -1,4 +1,7 @@
 <?php
+// Codeception
+require __DIR__.'/../c3.php';
+define('MY_APP_STARTED', true);
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
@@ -10,8 +13,8 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP']) 
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR']) 
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '10.0.0.1', '10.0.0.200'))
 ) {
     header('HTTP/1.0 403 Forbidden');
