c7e28862cc08427f4a35dcb0d9957fbbdc94ba87
Piotr Szymaszek <pszymaszek@gmail.com>
2017-08-04 15:28:34 +0200

    Better sidemenu toggle

--- assets/js/sidemenu.js
+++ assets/js/sidemenu.js
@@ -1,7 +1,12 @@
 var $ = require('jquery');
 
 $(document).ready(function() {
-    $('main').on('click', '.side-menu .fa', function () {
+    $('main').on('click', '.side-menu .side-menu-toggle', function () {
+        if ($('.side-menu').hasClass('expanded')) {
+            $(this).find('.fa').removeClass('fa-arrow-circle-right').addClass('fa-arrow-circle-left');
+        } else {
+            $(this).find('.fa').removeClass('fa-arrow-circle-left').addClass('fa-arrow-circle-right');
+        }
         $('.side-menu').toggleClass('expanded');
     });
 });
