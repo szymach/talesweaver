f7590cea59aa80fd1d9f62e40e72996797c1ee02
Piotr Szymaszek <pszymaszek@gmail.com>
2018-04-29 23:57:02 +0200

    Fixed top menu dropdown (with a workaround), removed it from xs view

--- assets/typescript/app.ts
+++ assets/typescript/app.ts
@@ -3,3 +3,10 @@ import './backdrop';
 import './display';
 import './alerts';
 import './lists';
+
+(function () {
+    $(function () {
+        // workaround dropdowns not working with data-toggle="dropdown"
+        $('.dropdown-toggle').dropdown();
+    });
+})();
