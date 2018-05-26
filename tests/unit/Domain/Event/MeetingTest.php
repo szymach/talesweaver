a41a2d7339436b5c864c1cb207094b241e961180
Piotr Szymaszek <pszymaszek@gmail.com>
2018-05-26 23:19:30 +0200

    Expanded Meeting unit tests

--- tests/unit/Domain/Event/MeetingTest.php
+++ tests/unit/Domain/Event/MeetingTest.php
@@ -12,7 +12,86 @@ use PHPUnit\Framework\TestCase;
 
 class MeetingTest extends TestCase
 {
-    public function testUserAccessWhenRootIsAnotherUsers()
+    public function testAccessDeniedToUserWhenNoFieldsSet()
+    {
+        $model = new Meeting();
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenOnlyRootSet()
+    {
+        $model = new Meeting();
+        $model->setRoot($this->createMock(Character::class));
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenOnlyRelationSet()
+    {
+        $model = new Meeting();
+        $model->setRelation($this->createMock(Character::class));
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenOnlyLocationSet()
+    {
+        $model = new Meeting();
+        $model->setLocation($this->createMock(Location::class));
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenRootAndLocationSet()
+    {
+        $user = $this->createMock(User::class);
+
+        $root = $this->createMock(Character::class);
+        $root->expects($this->exactly(3))->method('getCreatedBy')->willReturn($user);
+        $location = $this->createMock(Location::class);
+        $location->expects($this->exactly(1))->method('getCreatedBy')->willReturn($user);
+
+        $model = new Meeting();
+        $model->setRoot($root);
+        $model->setLocation($location);
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenRootAndRelationSet()
+    {
+        $user = $this->createMock(User::class);
+
+        $root = $this->createMock(Character::class);
+        $root->expects($this->exactly(3))->method('getCreatedBy')->willReturn($user);
+        $relation = $this->createMock(Character::class);
+        $relation->expects($this->exactly(1))->method('getCreatedBy')->willReturn($user);
+
+        $model = new Meeting();
+        $model->setRoot($root);
+        $model->setRelation($relation);
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testAccessDeniedToUserWhenRelationAndLocationSet()
+    {
+        $user = $this->createMock(User::class);
+
+        $root = $this->createMock(Character::class);
+        $root->expects($this->exactly(3))->method('getCreatedBy')->willReturn($user);
+        $location = $this->createMock(Location::class);
+        $location->expects($this->exactly(1))->method('getCreatedBy')->willReturn($user);
+
+        $model = new Meeting();
+        $model->setRoot($root);
+        $model->setLocation($location);
+
+        $this->assertFalse($model->isAllowed($this->createMock(User::class)));
+    }
+
+    public function testUserAccessDeniedWhenRootIsAnotherUsers()
     {
         $user = $this->createMock(User::class);
 
