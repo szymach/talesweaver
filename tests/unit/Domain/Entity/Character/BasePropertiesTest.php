23421f79981126b1e2ec4317f55bf90980c06778
Piotr Szymaszek <pszymaszek@gmail.com>
2018-05-29 23:00:25 +0200

    Split up CharacterTest class

--- tests/unit/Domain/Entity/Character/BasePropertiesTest.php
+++ tests/unit/Domain/Entity/Character/BasePropertiesTest.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Domain\Tests\Entity;
+namespace Domain\Tests\Entity\Character;
 
 use Assert;
-use Domain\Entity\Book;
-use Domain\Entity\Chapter;
 use Domain\Entity\Character;
 use Domain\Entity\Scene;
 use Domain\Entity\User;
@@ -14,7 +12,7 @@ use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\UuidInterface;
 
-class CharacterTest extends TestCase
+class BasePropertiesTest extends TestCase
 {
     public function testEmptyTitle()
     {
@@ -91,53 +89,5 @@ class CharacterTest extends TestCase
             $this->createMock(User::class)
         );
         $character->edit('', null, null);
-    }
-
-    public function testChapterIsAddedWhenSceneHasOne()
-    {
-        $chapter = $this->createMock(Chapter::class);
-        $scene = $this->createMock(Scene::class);
-        $scene->expects($this->exactly(2))->method('getChapter')->willReturn($chapter);
-
-        $currentScene = $this->createMock(Scene::class);
-        $currentScene->expects($this->once())->method('addCharacter')->with($this->isInstanceOf(Character::class));
-
-        $character = new Character(
-            $this->createMock(UuidInterface::class),
-            $currentScene,
-            'Chapter with two scenes',
-            '',
-            null,
-            $this->createMock(User::class)
-        );
-
-        $this->assertNotContains($scene, $character->getScenes()->toArray());
-        $this->assertNotContains($chapter, $character->getChapters()->toArray());
-        $character->addScene($scene);
-        $this->assertContains($scene, $character->getScenes()->toArray());
-        $this->assertContains($chapter, $character->getChapters()->toArray());
-    }
-
-    public function testBookIsSetWhenChapterHasOne()
-    {
-        $book = $this->createMock(Book::class);
-        $chapter = $this->createMock(Chapter::class);
-        $chapter->expects($this->exactly(4))->method('getBook')->willReturn($book);
-
-        $scene = $this->createMock(Scene::class);
-        $scene->expects($this->exactly(2))->method('getChapter')->willReturn($chapter);
-
-        $character = new Character(
-            $this->createMock(UuidInterface::class),
-            $this->createMock(Scene::class),
-            'Chapter with a book',
-            '',
-            null,
-            $this->createMock(User::class)
-        );
-
-        $this->assertNull($character->getBook());
-        $character->addScene($scene);
-        $this->assertEquals($book, $character->getBook());
     }
 }
