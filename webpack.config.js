4fcb5ad955fd1c9377605a136ecef2a72491064e
Piotr Szymaszek <pszymaszek@gmail.com>
2018-11-01 22:33:17 +0100

    Replaced UglifyJs with Terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const ClearWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
 const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         main: [
             path.resolve(__dirname, "node_modules/font-awesome/scss/font-awesome.scss"),
@@ -34,7 +34,7 @@ module.exports = {
         new MiniCssExtractPlugin({ filename: '[name].css' }),
         new OptimizeCssAssetsPlugin(),
         new CKEditorWebpackPlugin({ language: 'pl' }),
-        new ClearWebpackPlugin(['public/assets'], { root: __dirname, verbose: true }),
+        new ClearWebpackPlugin(['public/assets'], { root: __dirname, verbose: true })
     ],
     optimization: {
         minimizer: [
@@ -45,45 +45,15 @@ module.exports = {
                     }
                 }
             }),
-            new UglifyJsPlugin({
+            new TerserPlugin({
                 parallel: true,
                 sourceMap: true,
                 cache: true,
-                uglifyOptions: {
+                terserOptions: {
                     mangle: false,
                     warnings: false,
                     ecma: 6,
-                    sourceMap: true,
-                    compress: {
-                        // https://github.com/mishoo/UglifyJS2/tree/harmony#compress-options
-                        arrows: false,
-                        booleans: false,
-                        collapse_vars: false,
-                        comparisons: false,
-                        computed_props: false,
-                        conditionals: true,
-                        dead_code: true,
-                        evaluate: true,
-                        hoist_funs: false,
-                        hoist_props: false,
-                        hoist_vars: false,
-                        if_return: false,
-                        inline: false,
-                        join_vars: false,
-                        keep_infinity: true,
-                        loops: false,
-                        negate_iife: false,
-                        properties: false,
-                        reduce_funcs: false,
-                        reduce_vars: false,
-                        sequences: false,
-                        side_effects: false,
-                        switches: false,
-                        top_retain: false,
-                        toplevel: false,
-                        typeofs: false,
-                        unused: false,
-                    },
+                    sourceMap: true
                 }
             })
         ],
