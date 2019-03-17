d0663750882803e14a9513b526fdea0350101556
Piotr Szymaszek <pszymaszek@gmail.com>
2019-03-17 22:10:20 +0100

    Removed a bit of obsolete display javascript

--- assets/typescript/modules/display.ts
+++ assets/typescript/modules/display.ts
@@ -1,58 +1,36 @@
 const bootstrap = require('bootstrap.native');
 const Gator = require('gator');
-import { addClass, ajaxGetCall, trigger, removeClass } from '../common';
+import { ajaxGetCall, trigger } from '../common';
 import { AjaxContainer } from './ajaxContainer';
 
-interface DisplayResponse
-{
+interface DisplayResponse {
     display?: string | null
 }
 
-export module Display
-{
-    export function init(): void
-    {
+export module Display {
+    export function init(): void {
         Gator(document.querySelector('main')).on(
             'click',
             '.js-display',
-            (event : Event) => {
+            (event: Event) => {
                 const target = event.target as HTMLElement;
                 closeAllModals();
                 ajaxGetCall(
                     target.getAttribute('data-display-url'),
                     function (): void {
-                        const modal : HTMLElement = document.getElementById('modal-display');
-                        const response : DisplayResponse = this.response;
+                        const modal: HTMLElement = document.getElementById('modal-display');
+                        const response: DisplayResponse = this.response;
                         modal.querySelector('.modal-content').innerHTML = response.display;
-                        const modalClass : string = target.getAttribute('data-modal-class');
-                        if (null !== modalClass) {
-                            addClass(modal.querySelector('.modal-dialog'), modalClass);
-                            modal.setAttribute('data-class-to-remove', modalClass);
-                        }
                         new bootstrap.Modal(modal).show();
                     }
                 );
             }
         );
-
-        const modalDisplay = document.getElementById('modal-display');
-        if (null !== modalDisplay) {
-            document.getElementById('modal-display')
-                .addEventListener('hidden.bs.modal', (event: Event): void => {
-                    const target = event.target as HTMLElement;
-                    const classToRemove : string = target.getAttribute('data-class-to-remove');
-                    if (typeof classToRemove !== 'undefined') {
-                        removeClass(target.querySelector('.modal-dialog'), classToRemove);
-                        target.removeAttribute('data-class-to-remove');
-                    }
-                });
-        }
     }
 
-    export function closeAllModals(): void
-    {
+    export function closeAllModals(): void {
         document.querySelectorAll('.modal.in').forEach(
-            (element : Element) => {
+            (element: Element) => {
                 trigger(element.querySelector('[data-dismiss=modal]'), 'click');
             }
         );
