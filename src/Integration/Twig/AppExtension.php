6ea62badb925ddc22076a983d527a88667c014e2
Piotr Szymaszek <pszymaszek@gmail.com>
2019-04-20 23:51:54 +0200

    Active menu items by current route

---
+++ src/Integration/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talesweaver\Integration\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class AppExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction(
+                'isActiveMenuItem',
+                function (string $currentRoute, string $checkedRoute): bool {
+                    if ('book_list' === $checkedRoute && 0 === strpos($currentRoute, 'book_')) {
+                        $isActive = true;
+                    } elseif('chapter_list' === $checkedRoute && 0 === strpos($currentRoute, 'chapter_')) {
+                        $isActive = true;
+                    } elseif('scene_list' === $checkedRoute && 0 === strpos($currentRoute, 'scene_')) {
+                        $isActive = true;
+                    } else {
+                        $isActive = false;
+                    }
+
+                    return $isActive;
+                }
+            )
+        ];
+    }
+}
